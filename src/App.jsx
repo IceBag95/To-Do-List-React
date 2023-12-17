b2fcc3790f8f3e3d82f35e66eb43b7bcdbb05b25
Panagiotis Pagonis <pan.m.pagonis@gmail.com>
2023-12-17 20:10:08 +0200

    Functionality done! Time for the CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,106 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ListComp from './ListComp'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [totalTasks, setTotalTasks] = useState(0)
+  const [taskArray, setTaskArray] = useState([])
+  const [filteredTaskArray, setfilteredTaskArray] = useState([...taskArray]);
+
+  function onAddTask(){
+    const taskInput = document.getElementById('new-task-input')
+    if (taskInput.value.trim() !== ''){
+
+      const newTaskText = taskInput.value
+      taskInput.value = ""
+
+
+
+      // creating the new task object for the taskArray
+      const newTask = {
+        taskid: totalTasks,
+        task: newTaskText,
+        selected: false
+      }
+
+      setTotalTasks(t => t + 1)
+      setTaskArray(t => [...t, newTask])
+      setfilteredTaskArray(f => [...f, newTask])
+    }
+    else taskInput.value = ""
+  }
+
+  function onKeyDownAddTaskInput(event){
+    if (event.key === 'Enter'){
+      onAddTask()
+    }
+  }
+
+  function onSearch(){
+    const searchterm = document.getElementById('search-for-task').value
+
+    if (searchterm){
+      const updatedFilteredTaskArray = taskArray.filter((currenttask) => {
+                                                        return currenttask.task.toLowerCase().includes(searchterm.toLowerCase())
+                                                      })
+      setfilteredTaskArray(_ => updatedFilteredTaskArray)
+    }
+    else setfilteredTaskArray(_ => [...taskArray])
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id='app-container'>
+      <h1>My Tasks:</h1>
+      <div id='my-form'>
+        <div className='input-container'>
+          <div className='labels'>
+            <label htmlFor="new-task-input">New Task:</label>
+          </div>
+          <div className='inputs'>
+            <input  type="text" 
+                    id='new-task-input' 
+                    className='input'
+                    onKeyDown={(event) => onKeyDownAddTaskInput(event)}/>
+            <button className='button' 
+                    id='add-new-task-button'
+                    onClick={onAddTask}>
+                      Add task
+            </button> 
+          </div>
+        </div>
+        <div className='input-container'>
+          <div className='labels'>  
+            <label htmlFor="search-for-task">Search Task:</label>
+          </div>
+          <div className='inputs'>
+            <input  type="text" 
+                    id='search-for-task' 
+                    className='input'
+                    onChange={onSearch}/>
+            <button className='button' 
+                    id='search-button'
+                    onClick={onSearch}>
+                      Search
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div id='list-container'>
+        <ul>
+          {filteredTaskArray.length > 0 ? filteredTaskArray.map((_,index) =>{
+                                            return <li key={`filtered${index}`}>
+                                                      {<ListComp  taskArray={taskArray} 
+                                                                  filteredTaskArray={filteredTaskArray}
+                                                                  idx={index} 
+                                                                  setOgArray={setTaskArray}
+                                                                  setFiletredOgArray={setfilteredTaskArray}/>}
+                                                    </li>
+                                            }) : null}
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    
+    </div>
   )
 }
 
---
+++ src/ListComp.jsx
@@ -0,0 +1,53 @@
+import './ListComp.css'
+
+function ListComp(props){
+
+    const filteredTaskArray = [...props.filteredTaskArray]
+    const taskArray = [...props.taskArray]
+    const i = props.idx
+    // unfilterd_i -> taskid[k] == taskid[i] 
+
+    function changeSelectedState(){
+        const UpdatedFilteredTaskArray = [...filteredTaskArray]
+        UpdatedFilteredTaskArray[i].selected = !UpdatedFilteredTaskArray[i].selected
+
+        props.setFiletredOgArray(UpdatedFilteredTaskArray)
+    }
+
+    function deleteTask(){
+        console.log(filteredTaskArray)
+        console.log(taskArray)
+        let itemToDeleteTaskId;
+        const UpdatedFilteredTaskArray = filteredTaskArray.filter((_,index) => {
+                                                            itemToDeleteTaskId = filteredTaskArray[i].taskid
+                                                            return (filteredTaskArray[index].taskid !== filteredTaskArray[i].taskid)                                                                
+                                                        })
+        const updatedTaskArray = taskArray.filter((_, index) => {
+                                                return (taskArray[index].taskid !== itemToDeleteTaskId) 
+                                            })
+        console.log(UpdatedFilteredTaskArray)
+        console.log(updatedTaskArray)
+        props.setFiletredOgArray(UpdatedFilteredTaskArray)
+        props.setOgArray(updatedTaskArray)
+    }
+
+    return(
+        <div id='list-item'>
+            <input  type="checkbox" 
+                    id={`checkbox${i}`} 
+                    checked={filteredTaskArray[i].selected}
+                    onChange={changeSelectedState}/> 
+            <p id={`item-descr${i}`} 
+                className='item-descr'>
+                    {filteredTaskArray[i].selected ? <s>{filteredTaskArray[i].task}</s> : filteredTaskArray[i].task}           
+            </p>
+            <button id={`delete-item-btn${i}`} className='delete-item-btn' onClick={deleteTask}>
+                <span className="material-icons-round">
+                    delete_forever
+                </span>
+            </button>
+        </div>
+    )
+}
+
+export default ListComp
